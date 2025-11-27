5104ec573671822f08115e615e3840a12799c753
gnembhard <gnembbhard2020@fau.edu>
2025-11-27 17:47:32 -0500

    Initial Commit

---
+++ CarCare+/CarCare_App.swift
@@ -0,0 +1,17 @@
+//
+//  CarCare_App.swift
+//  CarCare+
+//
+//  Created by Giovanni Nembhard on 11/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CarCare_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarCare+Tests/CarCare_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  CarCare_Tests.swift
+//  CarCare+Tests
+//
+//  Created by Giovanni Nembhard on 11/27/25.
+//
+
+import Testing
+@testable import CarCare_
+
+struct CarCare_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
